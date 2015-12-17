e2686e6da174bf6c08b986ff74dfffc630a49517
Furmanator <dfurman@kfgisit.com>
2015-12-17 13:04:01 -0500

    Added Stylus Preprocessor support

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var elixir = require('laravel-elixir');
+require('laravel-elixir-stylus');
 
 /*
  |--------------------------------------------------------------------------
@@ -12,5 +13,5 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.stylus('app.styl');
 });
