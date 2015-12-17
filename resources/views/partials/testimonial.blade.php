efdc794107389e4bfe8a130e841046942151dd4d
Furmanator <dfurman@kfgisit.com>
2015-12-17 14:34:28 -0500

    Added Stylus styling elements in realistic workflow as an example of modern CSS3 workflow

---
+++ resources/views/partials/testimonial.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+<div class="Testimonial">
+    <h3 class="Testimonial__name"></h3>
+    <blockquote class="Testimonial__body">
+    </blockquote>
+
+    <div class="some-note utility-center"></div>
+
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,4 +4,12 @@
 
 @section('partials.section')
     <h1>Hello World</h1>
+
+    @foreach($testimonials as $testimony)
+        @include('testimonials/testimony')
+    @endforeach
+
+    <div class="Testimonial"></div>
+
+    <div class="Testimonial {{ $testimonial->featured ? 'Testimonial--is-featured': '' }}"></div>
 @stop
