8bfa25f8019bdd6f6509fbed6cea08279040b261
Furmanator <dfurman@kfgisit.com>
2015-12-17 14:44:34 -0500

    Finished up the basic stylus examples and proved that the gulp action is providing expected CSS preprocessing for some truly horrid looking styles, but hey; it proves the point!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,11 @@
 @section('partials.section')
     <h1>Hello World</h1>
 
-    @foreach($testimonials as $testimony)
-        @include('testimonials/testimony')
-    @endforeach
+    {{--@foreach($testimonials as $testimony)--}}
+        {{--@include('testimonials/testimony')--}}
+    {{--@endforeach--}}
 
     <div class="Testimonial"></div>
 
-    <div class="Testimonial {{ $testimonial->featured ? 'Testimonial--is-featured': '' }}"></div>
+    {{--<div class="Testimonial {{ $testimonial->featured ? 'Testimonial--is-featured': '' }}"></div>--}}
 @stop
