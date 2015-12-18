da716cdbbd976e366a6820e326555c88e0d43f20
Furmanator <dfurman@kfgisit.com>
2015-12-18 00:27:32 -0500

    Removed lorem text and added an example of flex-basis, which allows the flex content information to exist within grid examples.

--- resources/views/partials/grid.blade.php
+++ resources/views/partials/grid.blade.php
@@ -1,14 +1,17 @@
 <div class="grid">
+
     <div class="grid__row">
         <div class="grid__column">1</div>
         <div class="grid__column">2</div>
         <div class="grid__column">3</div>
         <div class="grid__column">4</div>
     </div>
+
     <div class="grid__row">
         <div class="grid__column">1</div>
         <div class="grid__column">2</div>
     </div>
+
     <div class="grid__row">
         <div class="grid__column">1</div>
         <div class="grid__column">2</div>
@@ -19,13 +22,19 @@
         <div class="grid__column">7</div>
         <div class="grid__column">8</div>
     </div>
+
     <div class="grid__row">
-        <div class="grid__column">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium animi cupiditate debitis, deserunt ducimus enim error et hic illo ipsam iste magni nobis numquam placeat possimus quo quod sit tempore.</div>
+        <div class="grid__column">1</div>
         <div class="grid__column">
             <div class="grid__row">
                 <div class="grid__column">2.1</div>
                 <div class="grid__column">2.2</div>
             </div>
         </div>
+    </div>
+
+    <div class="grid__row">
+        <div class="grid__column aside">Sidebar</div>
+        <div class="grid__column primary">Main Content</div>
     </div>
 </div>
